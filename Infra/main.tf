27aee857e1128ec61ffd98d8afb22997a1b32454
Aditya Mohapatra <75691553+Aditya5281@users.noreply.github.com>
2025-11-05 13:15:56 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,7 +1,7 @@
 # Dev Web App
 resource "azurerm_app_service_plan" "dev_plan" {
-    provider            = azurerm.Dev
-    name                = var.asp_dev
+    // provider            = azurerm.dev
+    name                = "asp-${terraform.workspace}"
     location            = data.azurerm_resource_group.dev_rg.location
     resource_group_name = data.azurerm_resource_group.dev_rg.name
     kind                = "Linux"
@@ -14,31 +14,29 @@ resource "azurerm_app_service_plan" "dev_plan" {
 }
 
 # Prod Web App
-resource "azurerm_app_service_plan" "prod_plan" {
-    provider            = azurerm.Prod
-    name                = var.asp_prod
-    location            = data.azurerm_resource_group.prod_rg.location
-    resource_group_name = data.azurerm_resource_group.prod_rg.name
-    kind                = "Linux"
-    reserved            = true
+// resource "azurerm_app_service_plan" "prod_plan" {
+//     // provider            = azurerm.Prod
+//     name                = "asp-${terraform.workspace}"
+//     location            = data.azurerm_resource_group.prod_rg.location
+//     resource_group_name = data.azurerm_resource_group.prod_rg.name
+//     kind                = "Linux"
+//     reserved            = true
 
-    sku {
-        tier = "Standard"
-        size = "S1"
-    }
-}
+//     sku {
+//         tier = "Standard"
+//         size = "S1"
+//     }
+// }
 
 # Dev Web App
 resource "azurerm_linux_web_app" "webapp" {
-    provider = azurerm.Dev
+    // provider            = azurerm.dev
     name                = var.webapp
-    location            = data.azurerm_resource_group.rg.location
-    resource_group_name = data.azurerm_resource_group.rg.name
+    location            = data.azurerm_resource_group.dev_rg.location
+    resource_group_name = data.azurerm_resource_group.dev_rg.name
     service_plan_id     = azurerm_app_service_plan.dev_plan.id
 
-    site_config {
-        linux_fx_version = "PYTHON:3.11"
-    }
+    site_config {}
 
     app_settings = {
         "WEBSITES_PORT" = "5000"
@@ -47,17 +45,23 @@ resource "azurerm_linux_web_app" "webapp" {
 
 # Azure Container Registry
 resource "azurerm_container_registry" "acr" {
-    provider = azurerm.Dev
+    // provider = azurerm.dev
     name = var.acr_name
     resource_group_name = data.azurerm_resource_group.dev_rg.name
     location = data.azurerm_resource_group.dev_rg.location
     sku = "Basic"
     admin_enabled = true
+    identity {
+      type = "UserAssigned"
+      identity_ids = [
+        data.azurerm_user_assigned_identity.mi.id
+      ]
+    }
 }
 
 # Azure Kubernetes Service
 resource "azurerm_kubernetes_cluster" "dev_aks" {
-  provider            = azurerm.Dev
+//   provider            = azurerm.dev
   name                = var.aks_name
   location            = data.azurerm_resource_group.dev_rg.location
   resource_group_name = data.azurerm_resource_group.dev_rg.name
@@ -73,11 +77,12 @@ resource "azurerm_kubernetes_cluster" "dev_aks" {
     type = "SystemAssigned"
   }
 
-  depends_on = [azurerm_container_registry.dev_acr]
+  depends_on = [azurerm_container_registry.acr]
 }
 
+// Attch ACR to AKS
 resource "azurerm_role_assignment" "aks_role" {
-    provider = azurerm.Dev
+    // provider = azurerm.dev
     scope = azurerm_container_registry.acr.id
     role_definition_name = "AcrPull"
     principal_id = azurerm_kubernetes_cluster.dev_aks.identity[0].principal_id
